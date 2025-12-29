81430f784b2474f34992a74d1137e7728467ab2d
Nguyễn Đặng Hoài Nam <stghoainam4002@gmail.com>
2025-12-29 21:12:03 +0700

    Feat: Initialized  LLM Builder

--- services/src/utils/user_util.py
+++ services/src/utils/user_util.py
@@ -2,9 +2,9 @@ import bcrypt
 from typing import TYPE_CHECKING
 
 # Only import for type checking, not at runtime
-if TYPE_CHECKING:
-    from src.modules.user.user_service import UserService
-    from src.modules.auth.dto.login_dto import LoginDto
+# if TYPE_CHECKING:
+#     from src.modules.user.user_service import UserService
+#     from src.modules.auth.dto.login_dto import LoginDto
 
 class UserUtil:
     def __init__(self):
@@ -18,6 +18,6 @@ class UserUtil:
     def comparePassword(self, password: str, hashed_password: str) -> bool:
         return bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
     
-    def validate_user(self, service: "UserService", data: "LoginDto"):
-        # Your validation logic here
-        return
+    # def validate_user(self, service: "UserService", data: "LoginDto"):
+    #     # Your validation logic here
+    #     return
