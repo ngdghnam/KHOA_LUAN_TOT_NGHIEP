73a0354fbc82d2a89ae5ffbf00301e7f86b81b9a
Nguyễn Đặng Hoài Nam <stghoainam4002@gmail.com>
2025-12-30 02:13:08 +0700

    Feat: setup FE Structure

---
+++ web/src/app/core/interceptors/http-error.interceptor.ts
@@ -0,0 +1,12 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+export const httpErrorInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req).pipe(
+    catchError((error) => {
+      console.error('HTTP Error:', error);
+      // Handle error here
+      return throwError(() => error);
+    })
+  );
+};
---
+++ web/src/app/layouts/blank-layout/blank-layout.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { BlankLayoutComponent } from './blank-layout.component';
+
+describe('BlankLayoutComponent', () => {
+  let component: BlankLayoutComponent;
+  let fixture: ComponentFixture<BlankLayoutComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BlankLayoutComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BlankLayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
