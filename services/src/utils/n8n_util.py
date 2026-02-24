6ff3b439fcc90adc99000193652e51b0270040fa
Nguyễn Đặng Hoài Nam <stghoainam4002@gmail.com>
2026-02-24 22:40:48 +0700

    Feat: change to production

--- services/src/utils/n8n_util.py
+++ services/src/utils/n8n_util.py
@@ -1,8 +1,8 @@
 import requests
 
-n8n_url = "http://localhost:5678/webhook-test/8c62f956-591f-4698-ab3d-b04f5b71b11d" 
-n8n_gen_sum_and_questions_url = "http://localhost:5678/webhook-test/c6cb8338-42ed-4c0d-a5b7-4bf9ed52ee0e"
-n8n_gen_following_question_url= "http://localhost:5678/webhook-test/cedff963-3e77-4f9d-b674-d0363039568b"
+n8n_url = "http://localhost:5678/webhook/8c62f956-591f-4698-ab3d-b04f5b71b11d" 
+n8n_gen_sum_and_questions_url = "http://localhost:5678/webhook/c6cb8338-42ed-4c0d-a5b7-4bf9ed52ee0e"
+n8n_gen_following_question_url= "http://localhost:5678/webhook/cedff963-3e77-4f9d-b674-d0363039568b"
 
 def post_data(data):
     return requests.post(
